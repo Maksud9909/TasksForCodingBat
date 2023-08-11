f3d96a9da36124fb255efb06a58004a47939252a
maksud9909 <rmaksud822@gmail.com>
2023-08-11 18:09:45 +0500

    Solved a leetcode

--- src/LeetCode/Search.java
+++ src/LeetCode/Search.java
@@ -1,7 +1,25 @@
 package LeetCode;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class Search {
-    public int search(int[] nums, int target) {
+    public static void main(String[] args) {
 
     }
+    public int search(int[] nums, int target) { // 1 2 3 4 5 target = 3
+        int index = 0;
+        for (int num:nums) {
+            if (num == target){
+               index =  Arrays.binarySearch(nums,target);
+               break;
+            }else {
+                index = -1;
+            }
+        }
+
+        return index;
+    }
 }
+
