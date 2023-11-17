d4f46c1d896dababe01b11837b50255d75577fc9
maksud9909 <rmaksud822@gmail.com>
2023-11-17 14:41:12 +0500

    Smth new

--- src/Array2/BigDiff.java
+++ src/Array2/BigDiff.java
@@ -1,8 +1,20 @@
 package Array2;
 
-public class BigDiff {
-    public int bigDiff(int[] nums) {
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
 
+public class BigDiff {
+    public static void main(String[] args) {
+        BigDiff bigDiff = new BigDiff();
+        bigDiff.bigDiff(new int[] {1,5,4});
+    }
+    public int bigDiff(int[] nums) {
+        int result = 0;
+        int max = Arrays.stream(nums).max().orElse(0);
+        int min = Arrays.stream(nums).min().orElse(0);
+        result =max - min;
+        return result;
     }
 
 }
--- src/String2/RepeatSeparator.java
+++ src/String2/RepeatSeparator.java
@@ -1,9 +1,6 @@
 package String2;
 
 public class RepeatSeparator {
-    public String repeatSeparator(String word, String sep, int count) {
-        String result = "";
 
-    }
 
 }
